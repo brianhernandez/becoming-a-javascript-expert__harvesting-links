9f64491b9c904655f108d6cbc6f3f94dee389f92
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-03-21 21:05:46 -0400

    Harvesting function working, output to JS console correct.

--- harvesting_links.js
+++ harvesting_links.js
@@ -1,21 +1,36 @@
 var harvestLinksButton = document.getElementById('submitButton');
 
 console.log("Harvesting Links file loaded successfully.");
-console.log(harvestLinksButton);
-
 
 harvestLinksButton.addEventListener('click', function(event){
   var emailAddressesArray = [];
-  var linksArray = {};
+  var linksArray = [];
+  var linksObject = {};
+  var tempArray = [];
+  var harvestedResults = {};
   var textareaValue = document.getElementById("textarea").value;
-
-  var emailPattern = /(?:<a\shref="mailto:)(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))(?:">)[\w\s\-]+(?:<\/a>)/g;
-  var emailPattern2 = /(?:<a\shref="mailto:)[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*(?:">)[\w\s\-]+(?:<\/a>)/g;
+  var emailPattern = /(?:<a\shref="mailto:)(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3})(?:">)[\w\s\-]+(?:<\/a>)/g;
+  var linkPattern = /(?:<a\shref=")(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*))(?:">)([\w\s\-]+)(?:<\/a>)/g;
   event.preventDefault();
 
+  while ((tempArray = emailPattern.exec(textareaValue)) !== null) {
+    var harvestedEmail = tempArray[1];
+    emailAddressesArray.push(harvestedEmail);
+  }
+  harvestedResults.emailAddresses = emailAddressesArray;
+  console.log(emailAddressesArray);
+  console.log(harvestedResults);
+  tempArray = [];
 
-  console.log(textareaValue);
-  console.log(emailPattern2.exec(textareaValue));
+  while ((tempArray = linkPattern.exec(textareaValue)) !== null) {
+    linksObject.linkText = tempArray[1];
+    linksObject.url = tempArray[2];
+    linksArray.push(linksObject);
+    linksObject = {};
+  }
+  harvestedResults.links = linksArray;
+  console.log(linksArray);
+  console.log(harvestedResults);
 });
 
 
