15e2c4095b120445d2d06fbc59917991db8f1083
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-03-26 15:26:44 -0400

    Completed project for now.

--- harvesting_links.js
+++ harvesting_links.js
@@ -1,39 +1,34 @@
 var harvestLinksButton = document.getElementById('submitButton'),
-    formElement = document.getElementById('form'),
-    clearFlag = false;
-    filledFlag = false;
-
-console.log("Harvesting Links file loaded successfully.");
+    formElement = document.getElementById('form');
 
 harvestLinksButton.addEventListener('click', function(event){
-  var emailAddressesArray = [];
-  var linksArray = [];
-  var linksObject = {};
-  var tempArray = [];
-  var harvestedResults = {};
-  var textareaValue = document.getElementById("textarea").value;
-  var emailPattern = /(?:<a\shref="mailto:)(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3})(?:">)[\w\s\-]+(?:<\/a>)/g;
-  var linkPattern = /(?:<a\shref=")(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*))(?:">)([\w\s\-]+)(?:<\/a>)/g;
-  var unorderedListElement = document.createElement("UL"),
+  var emailAddressesArray = [],
+      linksArray = [],
+      linksObject = {},
+      tempArray = [],
+      harvestedResults = {},
+      textareaValue = document.getElementById("textarea").value,
+      emailPattern = /(?:<a\shref="mailto:)(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3})(?:">)[\w\s\-]+(?:<\/a>)/g,
+      linkPattern = /(?:<a\shref=")(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*))(?:">)([\w\s\-]+)(?:<\/a>)/g,
+      unorderedListElement = document.createElement("UL"),
       unorderedListDivElement = document.createElement("DIV");
 
   event.preventDefault();
-  console.log(textarea);
+
   if (harvestLinksButton.className === "clearButton") {
     document.getElementById("textarea").value = "";
-    console.log(textareaValue + " NOTHING");
     harvestLinksButton.className = "";
     harvestLinksButton.innerHTML = "Harvest Links";
-    console.log(harvestLinksButton.innerHTML);
-    // if (document.getElementById("resultsDiv")) {
-      document.getElementById("resultsDiv").remove();
-    // }
+    document.getElementById("resultsDiv").remove();
   }
+
   if(textareaValue) {
+
     while ((tempArray = emailPattern.exec(textareaValue)) !== null) {
       var harvestedEmail = tempArray[1];
       emailAddressesArray.push(harvestedEmail);
     }
+
     harvestedResults.emailAddresses = emailAddressesArray;
     tempArray = [];
 
@@ -43,8 +38,8 @@ harvestLinksButton.addEventListener('click', function(event){
       linksArray.push(linksObject);
       linksObject = {};
     }
+
     harvestedResults.links = linksArray;
-    console.log(harvestedResults);
 
     if(harvestedResults) {
       if(harvestedResults.emailAddresses) {
@@ -72,7 +67,6 @@ harvestLinksButton.addEventListener('click', function(event){
         }
       }
     }
-    console.log(unorderedListElement);
     unorderedListDivElement.className = "linksList";
     unorderedListDivElement.id = "resultsDiv";
     unorderedListDivElement.appendChild(unorderedListElement);
